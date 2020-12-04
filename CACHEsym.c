c72c8d2f9fe4ee08b1b50cfa70a9e2fb4c5e6e1d
aparpo <alvaro.parpolowicz@live.u-tad.com>
2020-12-04 14:11:03 +0100

    Correción: añadida funcion startUp()
    Error en la entrada "añadida funcion startUp()". Error corregido.

--- CACHEsym.c
+++ CACHEsym.c
@@ -6,8 +6,8 @@ typedef struct {
  short int Datos[8];
 } T_LINEA_CACHE;
 
-void startUp();
-unsigned char[1024] startRAM();
+void startUp(T_LINEA_CACHE* cache, unsigned char* ram, FILE* f);
+unsigned char[1024] startRAM(FILE* f);
 void accessMemory(FILE* f);
 
 
@@ -27,6 +27,13 @@ int main(int argc, char** argv){
 	int numFallos = 0;
 	T_LINEA_CACHE chache[4];
 	unsigned char[1024] ram;
+	short int* siguienteAcceso;
+	FILE* fRAM;
+	FILE* fAccesos;
+	char texto[100];
+	
+	startUp(cache, ram, fRAM, fAccesos);
+	
 	
 	
 	
@@ -48,10 +55,24 @@ int main(int argc, char** argv){
 	
 }
 
-void startUp(T_LINEA_CACHE cache[4], unsigned char ram[1024]){
-	ram = startRam();
+void startUp(T_LINEA_CACHE* cache, unsigned char* ram, FILE* fRAM, FILE* fAccesos){
 	int i;
 	int j;
+	
+	fRAM = fopen("RAM.bin", "r");
+	fAccesos = fopen("accesos_memoria.txt", "r");
+	
+	if(RAM == null){
+		printf("No se ha podido abrir el fichero RAM.bin");
+		exit(-1);
+	}
+	if(accesos == null){
+		printf("No se ha podido abrir el fichero accesos_memoria.txt");
+		exit(-1);
+	}
+	
+	ram = startRam();
+	
 	for(i=0; i < 4; i++){
 		cache[i].ETQ = 0xFF;
 		for(j = 0; j < 8; j++){
