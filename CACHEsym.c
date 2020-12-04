c49b20725d2d677c9d289cbe1b2ca8b0e1393c28
aparpo <alvaro.parpolowicz@live.u-tad.com>
2020-12-04 13:38:12 +0100

    Planteamiento base
    Generación de variables, estructuras y funciones básicas.

--- CACHEsym.c
+++ CACHEsym.c
@@ -1,2 +1,49 @@
 // Alvaro Martinez 
 // Erick Cardenas
+
+typedef struct {
+ short int ETQ;
+ short int Datos[8];
+} T_LINEA_CACHE;
+
+void startUp();
+unsigned char[1024] startRAM();
+void accessMemory();
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(int argc, char** argv){
+	int tiempoGlobal = 0;
+	int numFallos = 0;
+	T_LINEA_CACHE chache[4];
+	unsigned char[1024] ram;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
